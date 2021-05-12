41445afc0d1acb8ae7ad691602f189679919c9c0
Alla <zhalla1999@mail.ru>
2021-05-13 01:25:19 +0300

    Оно наконец-то нормально считывает текст!!!!

--- Lab3/src/clientServer/Client.java
+++ Lab3/src/clientServer/Client.java
@@ -2,73 +2,96 @@ package clientServer;
 
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
+///TODO Чтение из файла
+
 public class Client {
-    public static void main(String[] args)
-    {
-        byte[] bKbdInput = new byte[256];
-        Socket s;
-        InputStream is;
-        OutputStream os;
-        try
-        {
-            System.out.println(
-                    "Socket Client Application" +
-                            "\nEnter any string or" +
-                            " 'quit' to exit...");
-        }
-        catch(Exception ioe)
-        {
-            System.out.println(ioe.toString());
-        }
-        try
-        {
-            s = new Socket("localhost",9999);
-            is = s.getInputStream();
-            os = s.getOutputStream();
-            byte[] buf = new byte[512];
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Socket socket;
+        InputStream inputStream;
+        OutputStream outputStream;
+        System.out.println(
+                "Socket Client Application" +
+                        "\nEnter any string or" +
+                        " 'quit' to exit...");
+        try {
+            socket = new Socket("localhost", 7777);
+            inputStream = socket.getInputStream();
+            outputStream = socket.getOutputStream();
             int length;
             String str;
-            while(true)
-            {
-                length = System.in.read(bKbdInput);
-                if(length != 1)
-                {
-                    str = new String(bKbdInput, 0);
-                    StringTokenizer st;
-                    st   = new StringTokenizer(str, "\r\n");
-                    str = new String((String)st.nextElement());
-                    System.out.println(">  " + str);
-                    os.write(bKbdInput, 0, length);
-                    os.flush();
-                    length = is.read(buf);
-                    if(length == -1)
+            while (true) {
+                byte[] bKbdInput = new byte[256];
+                byte[] buf = new byte[512];
+                String text = readText(in);
+
+                bKbdInput = text.getBytes(StandardCharsets.UTF_8);
+                length = bKbdInput.length;
+                if (length != 1) {
+
+                    /*Отправка на сервер*/
+                    System.out.print(">  " + text);
+                    outputStream.write(bKbdInput, 0, length);
+                    outputStream.flush();
+
+                    /*Чтение изменений*/
+                    length = inputStream.read(buf);
+                    if (length == -1)
                         break;
-                    str = new String(buf, 0);
-                    st   = new StringTokenizer(str, "\r\n");
-                    str = new String((String)st.nextElement());
+                    str = new String(buf, 0, buf.length);
+                    /*st = new StringTokenizer(str, "\r\n");
+                    str = (String) st.nextElement();*/
                     System.out.println(">> " + str);
-                    if(str.equals("quit")) break;
+
+                    if (str.equals("quit"))
+                        break;
                 }
             }
-            is.close();
-            os.close();
-            s.close();
-        }
-        catch(Exception ioe)
-        {
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+        } catch (Exception ioe) {
             System.out.println(ioe.toString());
         }
-        try
-        {
+        try {
             System.out.println(
                     "Press <Enter> to " + "terminate application...");
-            System.in.read(bKbdInput);
-        }
-        catch(Exception ioe)
-        {
+            in.nextLine();
+            in.nextLine();
+        } catch (Exception ioe) {
             System.out.println(ioe.toString());
         }
     }
+
+    public static String readText(Scanner in) {
+
+        System.out.print("Введите количество строк: ");
+        int n;
+        if (in.hasNextInt()) {
+            n = in.nextInt();
+        } else {
+            n = -1;
+            System.out.println("Ошибка ввода");
+        }
+        LinkedList<String> text = new LinkedList<>();
+        if (n > 0) {
+            System.out.println("Введите текст:");
+            in.nextLine();
+            for (int i = 0; i < n; i++)
+            {
+                String str = in.nextLine();
+                //System.out.println("** "+str);
+                text.add(str);
+            }
+
+        } else
+            text.add("quit");
+        //System.out.println("All: " + String.join(System.lineSeparator(), text));
+        return String.join(System.lineSeparator(), text);
+    }
+
 }
--- Lab3/src/clientServer/Server.java
+++ Lab3/src/clientServer/Server.java
@@ -4,62 +4,52 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
+///TODO Обработка текста
+///TODO Запись в файл
+
 public class Server {
-    public static void main(String[] args)
-    {
-        byte bKbdInput[] = new byte[256];
+
+    public static void main(String[] args) {
+
         ServerSocket ss;
         Socket s;
         InputStream is;
         OutputStream os;
-        try
-        {
-            System.out.println(
-                    "Socket Server Application");
-        }
-        catch(Exception ioe)
-        {
+        try {
+            System.out.println("Socket Server Application");
+        } catch (Exception ioe) {
             System.out.println(ioe.toString());
         }
-        try
-        {
-            ss = new ServerSocket(9999);
+        try {
+            ss = new ServerSocket(7777);
             s = ss.accept();
             is = s.getInputStream();
             os = s.getOutputStream();
-            byte buf[] = new byte[512];
-            int lenght;
-            while(true)
-            {
-                lenght = is.read(buf);
-                if(lenght == -1)
+            int length;
+            while (true) {
+                byte[] buf = new byte[512];
+                length = is.read(buf);
+                if (length == -1)
                     break;
-                String str = new String(buf, 0);
-                StringTokenizer st;
-                st   = new StringTokenizer(
-                        str, "\r\n");
-                str = new String(
-                        (String)st.nextElement());
+                String str = new String(buf, 0, buf.length);
+                //StringTokenizer st;
+                //st = new StringTokenizer(str, "\r\n");
+                //str = (String) st.nextElement();
                 System.out.println(">  " + str);
-                os.write(buf, 0, lenght);
+                os.write(buf, 0, buf.length);
                 os.flush();
             }
             is.close();
             os.close();
             s.close();
             ss.close();
-        }
-        catch(Exception ioe)
-        {
+        } catch (Exception ioe) {
             System.out.println(ioe.toString());
         }
-        try
-        {
+        try {
             System.out.println("Press Enter to terminate application...");
-            System.in.read(bKbdInput);
-        }
-        catch(Exception ioe)
-        {
+            System.in.read();
+        } catch (Exception ioe) {
             System.out.println(ioe.toString());
         }
     }
