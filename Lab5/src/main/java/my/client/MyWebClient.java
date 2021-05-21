27eabfc33a23028c5754406fe7e3c52cee723e86
Alla <zhalla1999@mail.ru>
2021-05-21 17:42:26 +0300

    Надо только протестировать правильность конвертирования

--- Lab5/src/main/java/my/client/MyWebClient.java
+++ Lab5/src/main/java/my/client/MyWebClient.java
@@ -1,24 +1,20 @@
 package main.java.my.client;
 
-// нужно, чтобы получить wsdl описание и через него
-// дотянуться до самого веб-сервиса
+
 import main.java.my.service.Measures;
 import main.java.my.service.OldRussianConverter;
 
 import java.net.URL;
-// такой эксепшн возникнет при работе с объектом URL
 import java.net.MalformedURLException;
-
-// классы, чтобы пропарсить xml-ку c wsdl описанием
-// и дотянуться до тега service в нем
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
-// интерфейс нашего веб-сервиса (нам больше и нужно)
-
-
 public class MyWebClient {
     public static void main(String[] args) throws MalformedURLException {
+        Scanner scanner = new Scanner(System.in);
         // создаем ссылку на wsdl описание
         URL url = new URL("http://localhost:8080/ws/hello?wsdl");
 
@@ -32,8 +28,57 @@ public class MyWebClient {
         // а далее и до вложенного в него тега port, чтобы
         // получить ссылку на удаленный от нас объект веб-сервиса
         OldRussianConverter hello = service.getPort(OldRussianConverter.class);
-
         // Ура! Теперь можно вызывать удаленный метод
-        System.out.println(hello.convert(4, Measures.TOCHKA, Measures.LINIA));
+        System.out.println("Выберите исходную меру:");
+        Measures from = inputMeasureType(scanner);
+        if (from != null) {
+            System.out.println("Выберите конечную меру:");
+            Measures to = inputMeasureType(scanner);
+            if (to != null) {
+                System.out.println("Введите число:");
+                double num;
+                if (scanner.hasNextDouble()) {
+                    num = scanner.nextDouble();
+                    System.out.print("Результат: ");
+                    System.out.println(hello.convert(num, from, to));
+                } else
+                    System.out.println("Ошибка ввода! Нужно ввести число");
+            }
+        }
     }
+
+    private static Measures inputMeasureType(Scanner scanner) {
+        System.out.println("1 - Точка");
+        System.out.println("2 - Линия");
+        System.out.println("3 - Вершок");
+        System.out.println("4 - Пядь");
+        System.out.println("5 - Аршин");
+        System.out.println("6 - Сажень");
+        System.out.println("7 - Верста");
+        int type = -1;
+        if (scanner.hasNextInt())
+            type = scanner.nextInt();
+        switch (type) {
+            case 0:
+                return null;
+            case 1:
+                return Measures.TOCHKA;
+            case 2:
+                return Measures.LINIA;
+            case 3:
+                return Measures.VERSHOK;
+            case 4:
+                return Measures.PYAD;
+            case 5:
+                return Measures.ARSHIN;
+            case 6:
+                return Measures.SAZHEN;
+            case 7:
+                return Measures.VERSTA;
+        }
+        System.out.println("Ошибка ввода! Неверное значение");
+        return null;
+    }
+
+
 }
